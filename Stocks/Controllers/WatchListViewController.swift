cc8d37438d62d012210bd8a3d5da0c523f685ca0
Amit Biswas <biswas1992@hotmail.com>
2021-07-12 01:51:22 -0400

    Update search result

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -11,10 +11,30 @@ class WatchListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        setUpSearchController()
     }
 
+    private func setUpSearchController() {
+        let resultVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
 
 }
 
+extension WatchListViewController: UISearchResultsUpdating {
+    // Search updating
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultVC = searchController.searchResultsController as? SearchResultsViewController,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        // Optimize to reduce number of searching  when users stop typing
+
+
+        // Call API Search
+    }
+}
