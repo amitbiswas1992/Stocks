45fed879af440a2671b72e427e02579ab1682786
Amit Biswas <biswas1992@hotmail.com>
2021-07-15 03:18:23 -0400

    Added Title View

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -9,12 +9,32 @@ import UIKit
 
 class WatchListViewController: UIViewController {
 
+    // MARK:- LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setUpSearchController()
+        setUpTitleView()
     }
 
+    // MARK:- Setup TitleView
+    
+    private func setUpTitleView() {
+        let titleView = UIView(
+            frame: CGRect(
+                x: 0,
+                y: 0,
+                width: view.widht,
+                height: navigationController?.navigationBar.height ?? 100
+            )
+        )
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: titleView.widht-20, height: titleView.height))
+        label.text = "Stocks"
+        label.font = .systemFont(ofSize: 32, weight: .medium)
+        titleView.addSubview(label)
+        navigationItem.titleView = titleView
+    }
+    
     private func setUpSearchController() {
         let resultVC = SearchResultsViewController()
         let searchVC = UISearchController(searchResultsController: resultVC)
